15be897beff4af0b9480567896175d81cafefd04
Wasihun Tesfaye <wasihunpersonal@gmail.com>
2025-02-02 15:27:44 +0300

    "Yolo object detection completed"

---
+++ telegram_data_dbt/target/compiled/telegram_data_dbt/models/final_models/schema.yml/not_null_telegram_metrics_messages_with_emoji.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select messages_with_emoji
+from "telegram_raw"."public"."telegram_metrics"
+where messages_with_emoji is null
+
+
---
+++ telegram_data_dbt/target/run/telegram_data_dbt/models/final_models/schema.yml/not_null_telegram_metrics_total_media_messages.sql
@@ -0,0 +1,19 @@
+select
+      count(*) as failures,
+      count(*) != 0 as should_warn,
+      count(*) != 0 as should_error
+    from (
+      
+    
+    
+
+
+
+select total_media_messages
+from "telegram_raw"."public"."telegram_metrics"
+where total_media_messages is null
+
+
+
+      
+    ) dbt_internal_test
